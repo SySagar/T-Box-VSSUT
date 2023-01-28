d00da877d61caea7b48d0abdc995cf08b2a29277
SySagar <sysagar07@gmail.com>
2023-01-28 21:13:29 +0530

    todo container

--- frontend/src/componenets/Todo.js
+++ frontend/src/componenets/Todo.js
@@ -7,6 +7,8 @@ import ListTasks from "../Todos/ListTasks";
 import InputTask from "../Todos/InputTask";
 import Storage from './Storage'
 
+import InnerHTML from 'dangerously-set-html-content'
+
 export default function Todo() {
   const [modal, setModal] = useState(false);
 
@@ -45,6 +47,37 @@ export default function Todo() {
     setTask({ tasks: result });
   };
 
+  const data = `<!DOCTYPE html>
+  <html lang="pt-br">
+  
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <script src="https://kit.fontawesome.com/1ab94d0eba.js" crossorigin="anonymous"></script>
+      <link rel="stylesheet" type="text/css" href="./style.css" />
+  
+      <title>TODO-LIST</title>
+  
+  </head>
+  
+  <body>
+      <div id="container">
+          <h1>TODO - LIST</h1>
+          <div id="box">
+              <input type="text" id="todo-input" name="todo" required placeholder="Add something..." autofocus>
+              <button type="button" id="btn-add"><i class="fas fa-plus"></i></button>
+              <button type="button" id="btn-del"><i class="fas fa-trash"></i></button>
+          </div>
+          <hr>
+          <ul id="todo-list"></ul>
+      </div>
+  
+      <script src="./script.js"></script>
+  </body>
+  
+  </html>`;
+
 
   const toggleModal = () => {
     setModal(!modal);
@@ -75,15 +108,7 @@ export default function Todo() {
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content">
-            <h2>Todos</h2>
-
-            <InputTask addTaskCallback={addTasks} />
-            <ListTasks tasksList={tasks["tasks"]} removeTaskCallback={removeTasks} strikeTaskCallback={strikeTask} />
-            <div className="close-modal">
-              <IconButton onClick={toggleModal} style={{ color: "white" }} aria-label="delete">
-                <ClearIcon />
-              </IconButton>
-            </div>
+          <InnerHTML html={data} />
           </div>
         </div>
       )}
